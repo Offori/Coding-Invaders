cba015a816f3fe592aab0edee64e5273340c34f4
Offori <61122385+Offori@users.noreply.github.com>
2023-07-28 22:07:25 +0530

    Add files via upload
    Collection

---
+++ collection/collection/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace chalange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var StudentName = new list<string>();
+            var StudentGrade = new list<int>();
+            var StudentCount = true;
+            while (StudentCount)
+            {
+                Console.WriteLine("what's your name ?");
+                StudentName.Add(Console.ReadLine());
+                Console.WriteLine("what's your grade ?");
+                StudentGrade.Add(int.Parse(Console.ReadLine()));
+                Console.WriteLine("Do you want to enter again a student? (y/n)");
+                if (Console.ReadLine() != "y")
+                {
+                    Console.WriteLine("thanks for registering");
+                    StudentCount = false;
+                    break;
+                }
+            }
+            for (int i = 0; i < StudentName.Count; i++)
+            {
+                Console.WriteLine("your details are follow : name : {0} , grade : {1}", StudentName[i], StudentGrade[i]);
+            }
+        }
+    }
+}
---
+++ collection/collection/list.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace chalange
+{
+    internal class list<T>
+    {
+        public list()
+        {
+        }
+
+        public int Count { get; internal set; }
+
+        internal void Add(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Add(int v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
